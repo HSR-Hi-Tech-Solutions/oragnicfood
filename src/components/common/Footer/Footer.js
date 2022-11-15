cac4873e9f84336413b355607c0a1fa6361b6434
HSR Hi-Tech Solutions <rajh91774@gmail.com>
2022-11-15 22:34:10 +0530

    navbar & footer

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -1,9 +1,97 @@
 import React from 'react';
+import { Container, Row,Col, InputGroup, Button, Form } from "react-bootstrap";
+import { ArrowRight } from "react-bootstrap-icons";
 import './Footer.scss';
+
+import { Envelope, HouseDoor } from "react-bootstrap-icons";
+
+import logo from '../../assets/logo-2.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <Container fluid className="hsr-footer">
+      <Container>
+        <Row>
+          <Col xs={12} md={3} lg={3}>
+            <img src={logo} alt={"Company Logo"} width={160}  />
+            <br />
+            <br/>
+            <p>Sed perspiciatis unde omnis natus error voluptatem accusan doloreqe laudantium totam aperiam eaque sipsa quae abillo inventore.</p>
+            <div className="mt-5">
+              <a href="#"> <Envelope /> &emsp;info@example.com</a>
+              <a href="#"><HouseDoor/> &emsp;Favicon, New York, USA - 25423</a>
+            </div>
+          </Col>
+          <Col xs={12} md={1} lg={2}></Col>
+          <Col xs={12} md={2} lg={2}>
+            <h5>Quick Links</h5>
+            <ul className="list-none">
+              <li>
+                <a href="#">About Our Company</a>
+              </li>
+              <li>
+                <a href="#">Wishlist</a>
+              </li>
+              <li>
+                <a href="#">Cart</a>
+              </li>
+              <li>
+                <a href="#">Flash Offers</a>
+              </li>
+              <li>
+                <a href="#">Terms & Conditions</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+            </ul>
+          </Col>
+          <Col xs={12} md={2} lg={2}>
+            <h5>Categories</h5>
+            <ul className="list-none">
+              <li>
+                <a href="#">Categories</a>
+              </li>
+              <li>
+                <a href="#">Wishlist</a>
+              </li>
+              <li>
+                <a href="#">Cart</a>
+              </li>
+              <li>
+                <a href="#">Flash Offers</a>
+              </li>
+              <li>
+                <a href="#">Terms & Conditions</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+            </ul>
+          </Col>
+          <Col xs={12} md={3} lg={3}>
+          <h5>Newsletter</h5>
+          <div className="mt-5">
+          <InputGroup className="hsr-search-section">
+        <Form.Control
+          placeholder="Enter Your Email"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+          className="hsr-search-input"
+        />
+        <Button id="button-addon2">
+          <div className="hsr-round"><ArrowRight  /></div>
+          
+        </Button>
+      </InputGroup>
+
+          </div>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+    </>
   )
 }
 
--- src/components/common/MainNav/MainNav.js
+++ src/components/common/MainNav/MainNav.js
@@ -1,9 +1,57 @@
 import React from 'react';
 import './MainNav.scss';
+import {Navbar, Container, NavDropdown,Nav,InputGroup,Form, Button, Row, Col} from 'react-bootstrap';
+import {Cart4, Search} from 'react-bootstrap-icons';
+
+import logo from '../../assets/logo-2.png';
 
 const MainNav = () => {
   return (
-    <div>MainNav</div>
+    <>
+    <Navbar className="hsr-navbar" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={logo} alt={"Company Logo"} width={160} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className=" ms-auto me-auto">
+            <Nav.Link className="hsr-menus" href="/home">Home</Nav.Link>
+            <Nav.Link className="hsr-menus" href="/about">About</Nav.Link>
+            <NavDropdown className="hsr-menus" title="Fruits & Vegetables" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Dry Fruits</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Seasonal Fruits
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Vegetables</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                All Fruits & Vegetables
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Row>
+            <Col lg={12} md={12}>
+            <InputGroup className="hsr-search-section">
+        <Form.Control
+          placeholder="Select products..."
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+          className="hsr-search-input"
+        />
+        <Button id="button-addon2">
+          <Search />
+        </Button>
+      </InputGroup>
+            </Col>
+            </Row>
+      &emsp;
+
+          <Nav.Link className="hsr-menus" href="/cart" ><Cart4 /></Nav.Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
   )
 }
 
